2b6da0da1290389682738d04a4bd0710850f8c4a
devendra <devendra7023663494@gmail.com>
2022-05-28 23:44:28 +0530

    reactassess5

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Higher Order Component</h1>
+      <p>A higher-order component (HOC) is an advanced technique in React for reusing component logic.</p>
+      <p>HOC is a function that takes a component and returns a new component.</p>
+      <h1>Pure Component</h1>
+      <p>A React component is considered pure if it renders the same output for the same state and props.</p>
+      <p>Component Class compares current state and props with new props and states to decide whether the React component should re-render itself or Not.</p>
+      <p>If the previous value of state or props and the new value of state or props is same then component will not re-render.</p>
+    </div>
+  );
+}
+
+export default App;
